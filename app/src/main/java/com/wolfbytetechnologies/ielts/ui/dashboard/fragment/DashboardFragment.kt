39f298a5a4bdcf79da9a6178590e9dbd4412ac7a
Filiksyos Destaw <franknick285@gmail.com>
2024-12-01 17:26:34 +0300

    just commit

--- app/src/main/java/com/wolfbytetechnologies/ielts/ui/dashboard/fragment/DashboardFragment.kt
+++ app/src/main/java/com/wolfbytetechnologies/ielts/ui/dashboard/fragment/DashboardFragment.kt
@@ -34,51 +34,14 @@ class DashboardFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
     private lateinit var mainDashboardItemsRepo: MainDashboardItemsRepo
     private lateinit var dashboardAdapter: DashboardAdapter
-    private var mInterstitialAd: InterstitialAd? = null
-    private val tag = "AdLog"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        onBackPressed()
-        loadInterstitialAd()
+
+
     }
 
-    private fun onBackPressed() {
-        requireActivity().onBackPressedDispatcher.addCallback(
-                this,
-                object : OnBackPressedCallback(true) {
-                    override fun handleOnBackPressed() {
-
-                        val customDialogView = LayoutInflater.from(requireContext())
-                            .inflate(R.layout.custom_dialog_layout, null)
-                        val customDialogViewBinding =
-                            CustomDialogLayoutBinding.bind(customDialogView)
-                        val alertDialogBuilder = MaterialAlertDialogBuilder(
-                            requireContext(),
-                            R.style.MyRounded_MaterialComponents_MaterialAlertDialog
-                        ).setView(customDialogView).setCancelable(false)
-
-                        val alertDialog = alertDialogBuilder.show()
-
-                        customDialogViewBinding.btnNo.setOnClickListener {
-                            if (mInterstitialAd != null) {
-                                showInterstitialAd()
-                            } else {
-                                Log.d("TAG", "The interstitial ad wasn't ready yet.")
-                            }
-                            alertDialog.dismiss()
-                        }
-                        customDialogViewBinding.btnYes.setOnClickListener {
-                            if (mInterstitialAd != null) {
-                                showInterstitialAd()
-                            } else {
-                                requireActivity().finish()
-                            }
-                            requireActivity().finish()
-                        }
-                    }
-                })
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -100,66 +63,6 @@ class DashboardFragment : Fragment() {
 
         observeDashboardItems()
 
-        binding.apply {
-            btnVocabulary.setOnClickListener {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
-                displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToVocabularyTopicsFragment())
-            }
-            btnTips.setOnClickListener {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToIeltsTipsFragment())
-            }
-            btnGrammar.setOnClickListener {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToGrammarHomeFragment())
-            }
-        }
-    }
-
-    private fun showInterstitialAd() {
-        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
-            override fun onAdClicked() {
-                // Called when a click is recorded for an ad.
-                Log.d(tag, "Ad was clicked.")
-            }
-
-            override fun onAdDismissedFullScreenContent() {
-                // Called when ad is dismissed.
-                Log.d(tag, "Ad dismissed fullscreen content.")
-                mInterstitialAd = null
-            }
-
-            override fun onAdFailedToShowFullScreenContent(p0: AdError) {
-                // Called when ad fails to show.
-                Log.e(tag, "Ad failed to show fullscreen content.")
-                mInterstitialAd = null
-            }
-
-            override fun onAdImpression() {
-                // Called when an impression is recorded for an ad.
-                Log.d(tag, "Ad recorded an impression.")
-            }
-
-            override fun onAdShowedFullScreenContent() {
-                // Called when ad is shown.
-                Log.d(tag, "Ad showed fullscreen content.")
-            }
-        }
-
-        mInterstitialAd?.show(requireActivity())
     }
 
     private fun observeDashboardItems() {
@@ -179,102 +82,48 @@ class DashboardFragment : Fragment() {
     private fun onClick(adapterPosition: Int) {
         when (adapterPosition) {
             0 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToReadingLessonHomeFragment())
+                //The intent navigation
             }
 
             1 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToReadingTestMainTopicsFragment())
+                //The intent navigation
             }
 
             2 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                    loadInterstitialAd()
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToWritingLessonHomeFragment())
+                //The intent navigation
             }
 
             3 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(
-                    DashboardFragmentDirections.actionDashboardFragmentToWritingTasksFragment(
-                        "Writing Task 1"
-                    )
-                )
+                //The intent navigation
             }
 
             4 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(
-                    DashboardFragmentDirections.actionDashboardFragmentToWritingTasksFragment(
-                        "Writing Task 2"
-                    )
-                )
+                //The intent navigation
             }
 
             5 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToSpeakingLessonHomeFragment())
+                //The intent navigation
             }
 
             6 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToSpeakingTestHomeFragment())
+                //The intent navigation
             }
 
             7 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToListeningLessonHomeFragment())
+                //The intent navigation
             }
 
             8 -> {
-                if (mInterstitialAd != null) {
-                    showInterstitialAd()
-                } else {
-                    Log.d(tag, "The interstitial ad wasn't ready yet.")
-                }
                 displayProgressIfSlow()
-                findNavController().navigate(DashboardFragmentDirections.actionDashboardFragmentToListeningTestMainTopicsFragment())
+                //The intent navigation
             }
         }
     }
@@ -290,26 +139,7 @@ class DashboardFragment : Fragment() {
             requireContext(), getString(R.string.ielts_preparation_actionbar)
         )
         dashboardViewModel.saveButtonVisibility(requireContext(), false)
-        loadInterstitialAd()
     }
 
 
-    private fun loadInterstitialAd() {
-        val adRequest = AdRequest.Builder().build()
-
-        InterstitialAd.load(requireContext(),
-            "ca-app-pub-1254010252723206/6027590401",
-            adRequest,
-            object : InterstitialAdLoadCallback() {
-                override fun onAdFailedToLoad(adError: LoadAdError) {
-                    Log.d(tag, adError.toString())
-                    mInterstitialAd = null
-                }
-
-                override fun onAdLoaded(interstitialAd: InterstitialAd) {
-                    Log.d(tag, "Ad was Loaded")
-                    mInterstitialAd = interstitialAd
-                }
-            })
-    }
 }
